0bb9aaf38f8c448b84ce1ebf9b143cc438e49f22
Vinicius Souza <vinicius.souza@nre.tec.br>
2022-10-11 10:08:32 -0300

    init carditem :constructions:

--- src/components/characters/CardItem.vue
+++ src/components/characters/CardItem.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="card-item-component">
-    
+    <div class="custom-card">
+      <div class="custom-header">
+        <img src="" alt="">
+      </div>
+    </div>
   </div>
 </template>
+<style lang="scss" scoped>
+  .card-item-component{
+    .custom-card{
+      border-radius: 16px;
+      background: #f0f0f0;
+
+    }
+  }
+</style>
